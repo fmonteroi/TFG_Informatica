1b9900f2a82857b0eac29a47204f8f6280eabfc8
fmonteroi <fmonteroi@alumnos.unex.es>
2026-02-05 16:25:20 +0100

    Commit inicial

---
+++ backend/src/test/java/es/unex/cume/tfg/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package es.unex.cume.tfg.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
